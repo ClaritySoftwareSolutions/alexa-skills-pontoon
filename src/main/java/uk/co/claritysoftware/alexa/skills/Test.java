887cfe5c899f70794a0066c71b856c440cafd05f
Nathan Russell <nathan@claritysoftware.co.uk>
2019-06-05 12:21:20 +0100

    Files to support timing app startup and execution

---
+++ src/main/java/uk/co/claritysoftware/alexa/skills/Test.java
@@ -0,0 +1,59 @@
+package uk.co.claritysoftware.alexa.skills;
+
+import java.util.UUID;
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.speechlet.Context;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import uk.co.claritysoftware.alexa.skills.pontoon.dagger.DaggerPontoonComponent;
+import uk.co.claritysoftware.alexa.skills.pontoon.dagger.PontoonComponent;
+import uk.co.claritysoftware.alexa.skills.pontoon.speech.PontoonSpeechlet;
+
+/**
+ * @author Nathan Russell
+ */
+public class Test {
+
+	public static void main(String[] args) {
+
+		PontoonComponent pontoonComponent = DaggerPontoonComponent.create();
+		PontoonSpeechlet pontoonSpeechlet = pontoonComponent.buildPontoonSpeechlet();
+
+		// Start Session
+		SpeechletRequestEnvelope requestEnvelope = sessionStartedSpeechletRequestEnvelope();
+		pontoonSpeechlet.onSessionStarted(requestEnvelope);
+		Session session = requestEnvelope.getSession();
+
+		// Launch
+		requestEnvelope = launchSpeechletRequestEnvelopeWithSession(session);
+		SpeechletResponse speechletResponse = pontoonSpeechlet.onLaunch(requestEnvelope);
+
+		System.out.println("OutputSpeech = " + ((PlainTextOutputSpeech)speechletResponse.getOutputSpeech()).getText());
+
+	}
+
+	private static SpeechletRequestEnvelope<SessionStartedRequest> sessionStartedSpeechletRequestEnvelope() {
+		return SpeechletRequestEnvelope.<SessionStartedRequest>builder()
+				.withVersion("1.0")
+				.withContext(Context.builder().build())
+				.withSession(Session.builder().withSessionId(UUID.randomUUID().toString()).build())
+				.withRequest(SessionStartedRequest.builder()
+						.withRequestId(UUID.randomUUID().toString())
+						.build())
+				.build();
+	}
+
+	private static SpeechletRequestEnvelope<LaunchRequest> launchSpeechletRequestEnvelopeWithSession(final Session session) {
+		return SpeechletRequestEnvelope.<LaunchRequest>builder()
+				.withVersion("1.0")
+				.withContext(Context.builder().build())
+				.withSession(session)
+				.withRequest(LaunchRequest.builder()
+						.withRequestId(UUID.randomUUID().toString())
+						.build())
+				.build();
+	}
+}
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+time java -cp ./target/alexa-skills-pontoon-1.0.4-SNAPSHOT.jar uk.co.claritysoftware.alexa.skills.Test
