c6929face5ee95c29f5149e73d2ed8b755930656
Nathan Russell <nathan@claritysoftware.co.uk>
2019-06-10 20:26:05 +0100

    Added main class to the jar manifest

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-time java -cp ./target/alexa-skills-pontoon-1.0.4-SNAPSHOT.jar uk.co.claritysoftware.alexa.skills.Test
+time java -jar ./target/alexa-skills-pontoon-1.0.4-SNAPSHOT.jar
+
